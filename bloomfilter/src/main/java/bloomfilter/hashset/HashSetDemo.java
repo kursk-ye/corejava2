c793b29084dc92b878f4555f7d5f5d6650eb009e
kursk-ye <kursk.ye@gmail.com>
2022-12-09 14:59:06 +0800

    init

---
+++ bloomfilter/src/main/java/bloomfilter/hashset/HashSetDemo.java
@@ -0,0 +1,38 @@
+package bloomfilter.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+  static Set<String> backListIPs = new HashSet<String>();
+  static final long total = 100_000_000L;
+
+  static Set<String> pushList(Set<String> list) {
+
+    for (int i = 0; i < total; i++) {
+      list.add("10.255.255." + i);
+    }
+
+    return list;
+  }
+
+  public static void main(String[] args) {
+    //
+    backListIPs = pushList(new HashSet<String>());
+    long start = System.nanoTime();
+    System.out.println(backListIPs.contains("10.255.255.100"));
+    long elapsed = System.nanoTime() - start;
+    System.out.println("elapsed in nanos: " + elapsed);
+
+    for (; ; ) {}
+  }
+
+/*
+total                       |  JVM used heap size  |  elapsed in nanos
+100_000L             |    47.7MBytes             | 428500
+1_000_000L         |     145MBytes             | 288500
+10_000_000L      |     1.2GBytes                |
+100_000_000L    |     >4GBytes
+*/
+
+}
